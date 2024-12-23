d5d33ff763ee2edd12502c652dea04f8f7f0a8e4
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-23 17:28:28 +0545

    Questions>printing_variables.py file created; given question solved out

---
+++ variables/Questions/printing_variables.py
@@ -0,0 +1,17 @@
+"""
+Write a Python program that does the following:
+
+1. Define variables for your name, age, and favorite programming language.
+2. Use appropriate data types for each variable.
+3. Print a message introducing yourself using these variables, formatted as:
+    "Hi, my name is [Name], I am [Age] years old, and my favorite programming language is [Programming Language]."
+"""
+
+name = "Nishan Limbu"
+age = 24
+programming = "Python"
+
+print(f"Hi, my name is {name}, I am {age} years old, and my favorite programming language is {programming}.")
+
+# Output:
+    # Hi, my name is Nishan Limbu, I am 24 years old, and my favorite programming language is Python.
