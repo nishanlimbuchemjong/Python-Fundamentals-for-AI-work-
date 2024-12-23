b47d26699a2ce588ae7a57aeeb1fcb015d9e6938
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-23 15:30:08 +0545

    data types> different_dataTypes.py file crated and explained about data types in python

---
+++ data types/different_dataTypes.py
@@ -0,0 +1,15 @@
+"""
+Python Data Types:
+    Data Types are the classification or categorization of data items. 
+    It represents the kind of value that tells what operations can be performed on a particular data. 
+    Since everything is an object in Python programming, Python data types are classes and variables 
+    are instances (objects) of these classes. 
+
+    The following are the standard or built-in data types in Python:
+    Numeric -> int, float, complex
+    Sequence Type -> string, list, tuple
+    Mapping Type -> dict
+    Boolean -> bool
+    Set Type -> set, frozenset
+    Binary Types -> bytes, bytearray, memoryview
+"""
